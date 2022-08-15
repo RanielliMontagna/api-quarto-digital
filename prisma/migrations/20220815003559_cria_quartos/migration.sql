920643c963c68026ba94966fcada81365c4be7b4
Ranielli <raniellimontagna@hotmail.com>
2022-08-14 21:36:52 -0300

    feat(prisma): implementa nova tabela para funcionalidade de quartos

---
+++ prisma/migrations/20220815003559_cria_quartos/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "quartos" (
+    "id" SERIAL NOT NULL,
+    "identificacao" TEXT NOT NULL,
+    "tipo" TEXT,
+    "diaria" DOUBLE PRECISION NOT NULL,
+    "status" INTEGER NOT NULL DEFAULT 0,
+    "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "alteradoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "usuarioId" INTEGER,
+
+    CONSTRAINT "quartos_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "quartos" ADD CONSTRAINT "quartos_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "usuarios"("id") ON DELETE SET NULL ON UPDATE CASCADE;
